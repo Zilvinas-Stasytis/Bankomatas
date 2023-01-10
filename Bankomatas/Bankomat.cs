ee060d7cf32fd49d58bef8fcced67eced06521cb
Žilvinas Stasytis <zalciukas@gmail.com>
2023-01-10 21:17:01 +0200

    Algoritmo patikrinimui

--- Bankomatas/Bankomat.cs
+++ Bankomatas/Bankomat.cs
@@ -4,10 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bankomatas1
+namespace Bankomatas
 {
     public class Bankomat
     {
 public string PAN { get; set; }
+       public static void addCard()
+        {
+            List<string> listPans = new List<string>();
+            for (int i = 0; i<6; i++)
+            {
+                Console.WriteLine("Įvesk PAN");
+                Banque.PAN = Console.ReadLine();
+                listPans.Add(Banque.PAN);
+            }
+            foreach (string pan in listPans)
+            {
+                Console.WriteLine($"{pan}");
+            }
+        }
     }
 }
--- Bankomatas/Program.cs
+++ Bankomatas/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Bankomatas1;
-using Kortele;
-using Pervedimas;
+using Bankomatas;
+
+
 
     static void Main()
     {
         Bankomat Banque = new Bankomat();
         Card debetCard = new Card();
         Transaction transakcija = new Transaction();
+    addCard();
     }
+
